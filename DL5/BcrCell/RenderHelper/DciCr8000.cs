baad726c7d92e011ada74b9eba97dfc4cdd70e75
Edward Dwayne Rush <edward.rush.developer@live.com>
2018-01-18 07:23:01 -0500

    Initial Commit

---
+++ DL5/BcrCell/RenderHelper/DciCr8000.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+namespace Com.Cell.Device.Bcr.Dci.Code
+{
+    public partial class DciCr8000
+    {
+        public IntPtr Handle => throw new NotImplementedException();
+
+        public void Dispose()
+        {
+           
+        }
+    }
+}
